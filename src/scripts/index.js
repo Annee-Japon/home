74f21ae64ace138102f248f532d3e6f2d6b61a42
lheido <leonard.treille@gmail.com>
2020-02-06 19:00:27 +0100

    Update style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,23 @@
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+window.addEventListener('DOMContentLoaded', () => {
+    // Animation on enter (page load or scroll, the first time only).
+    if ('IntersectionObserver' in window) {
+        const animatable = document.querySelectorAll('[data-animate]');
+        animatable.forEach((element) => {
+            const observer = new IntersectionObserver((entries) => {
+                entries.forEach((entry) => {
+                    if (entry.isIntersecting) {
+                        entry.target.classList.add('animate');
+                    }
+                });
+            }, {
+                root: null,
+                threshold: [0.5]
+            });
+            observer.observe(element);
+        });
+    } else {
+        document.body.classList.add('no-animation');
+    }
+});
