634d6b45e34e02afcc247789ec392cb1476518d3
lheido <leonard.treille@gmail.com>
2022-05-22 13:41:05 +0000

    build: Update dependencies + add gitpod support

---
+++ .gitpod.dockerfile
@@ -0,0 +1,3 @@
+FROM gitpod/workspace-full
+
+RUN npm install -g webpack-cli webpack webpack-dev-server cross-env
--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,43 +1,47 @@
-const Path = require('path');
-const Webpack = require('webpack');
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
+const Path = require("path");
+const Webpack = require("webpack");
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
 
 module.exports = merge.merge(common, {
-  mode: 'development',
-  devtool: 'eval-cheap-source-map',
+  mode: "development",
+  devServer: {
+    host: "0.0.0.0",
+    allowedHosts: ["localhost", ".gitpod.io"],
+  },
+  devtool: "eval-cheap-source-map",
   output: {
-    chunkFilename: 'js/[name].chunk.js'
+    chunkFilename: "js/[name].chunk.js",
   },
   plugins: [
     new Webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: Path.resolve(__dirname, '../src'),
-        enforce: 'pre',
-        loader: 'eslint-loader',
+        include: Path.resolve(__dirname, "../src"),
+        enforce: "pre",
+        loader: "eslint-loader",
         options: {
           emitWarning: true,
-        }
+        },
       },
       {
         test: /\.js$/,
-        include: Path.resolve(__dirname, '../src'),
-        loader: 'babel-loader'
+        include: Path.resolve(__dirname, "../src"),
+        loader: "babel-loader",
       },
       {
         test: /\.s?css$/i,
         use: [
-          'style-loader',
-          {loader: 'css-loader', options: {url: false}},
-          'sass-loader'
-        ]
-      }
-    ]
-  }
+          "style-loader",
+          { loader: "css-loader", options: { url: false } },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 });
